9cdbdfffdab936f7b1cf3edd502b301403399704
Júlio C. Oliveira <juliocoliveira@users.noreply.github.com>
2016-07-29 16:01:54 -0300

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# NexusGateway
-Players join manager
+Nexus Gatekeeper
+=====
+
+Nexus Gatekeeper is a player Country Blocker for Garry's Mod, Not just that is a full set of player join controller
---
+++ lua/autorun/sh_ngk_loader.lua
@@ -0,0 +1,30 @@
+-- Nexus Gatekeeper
+-- Copyright (c) 2013 Nexus [BR] <http://www.nexusbr.net>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- $Id$
+-- Version 2.0.5 - 10-11-2013 10:53 PM
+
+
+if SERVER then
+  AddCSLuaFile()
+  AddCSLuaFile("nexus_gatekeeper/core_shared.lua") 
+end
+
+hook.Add("Initialize", "NGKLoader", function ()
+	if NexusGK == nil then
+		include("nexus_gatekeeper/core_shared.lua")
+	end
+end)
