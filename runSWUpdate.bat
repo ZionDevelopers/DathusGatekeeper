1f14360306b7a6244a12c6646e0dfa7fb6948b52
Júlio C. Oliveira <talk@juliocesar.me>
2023-06-07 02:47:45 -0300

    Update runSWUpdate.bat

--- runSWUpdate.bat
+++ runSWUpdate.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d G:\SteamLibrary\SteamApps\common\GarrysMod\bin
-gmad.exe create -folder "D:/Github/NexusGatekeeper" -out "D:/Github/NexusGatekeeper.gma"
-gmpublish.exe update -addon "D:/Github/NexusGatekeeper.gma" -id 734208849
+cd /d V:\SteamLibrary\SteamApps\common\GarrysMod\bin
+gmad.exe create -folder "D:/Github/DathusGatekeeper" -out "D:/Github/DathusGatekeeper.gma"
+gmpublish.exe update -addon "D:/Github/DathusGatekeeper.gma" -id 734208849
 pause
